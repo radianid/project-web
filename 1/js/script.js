dbf124e835df7a9436adaecfdc6c58a979c8725c
radianid <54707882+radianid@users.noreply.github.com>
2019-11-25 19:32:25 +0700

    Add files via upload

---
+++ 1/js/script.js
@@ -0,0 +1,11 @@
+$(window).on('load', function() {
+    $('.huruf').addClass('hMuncul');
+    $('.logo').addClass('lMuncul');
+    $('.link').addClass('uMuncul');
+    $('.brand').addClass('bMuncul');
+    $('.cKiri').addClass('aMuncul');
+    $('.cKanan').addClass('aMuncul');
+    $('.title').addClass('tMuncul');
+    $('.p').addClass('pMuncul');
+
+});
